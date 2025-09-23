72984c34b9664c69c242aa224e3059abbf2aa34f
Hahinika Dilrukshi <ghashinika2002@gmail.com>
2025-09-23 09:03:21 +0530

    ....

--- environment/environment.ts
+++ environment/environment.ts
@@ -1,8 +1,8 @@
 // environment.ts
 const environment = {
-        API_BASE_URL: "http://192.168.8.180:3000/agro-api/salesdash/",
+     //   API_BASE_URL: "http://192.168.1.14:3000/agro-api/salesdash/",
   // API_BASE_URL: "https://polygonagro.com/dash-api/agro-api/salesdash/",
-//API_BASE_URL: "https://dev.polygonagro.com/dash-api/agro-api/salesdash/",
+API_BASE_URL: "https://dev.polygonagro.com/dash-api/agro-api/salesdash/",
     SHOUTOUT_API_KEY:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIwYjUxMWUzMC1mMjZjLTExZWYtOGQyZi1jNzhmOWJkMzU4NzciLCJzdWIiOiJTSE9VVE9VVF9BUElfVVNFUiIsImlhdCI6MTc0MDM3MzEzMywiZXhwIjoyMDU1OTA1OTMzLCJzY29wZXMiOnsiYWN0aXZpdGllcyI6WyJyZWFkIiwid3JpdGUiXSwibWVzc2FnZXMiOlsicmVhZCIsIndyaXRlIl0sImNvbnRhY3RzIjpbInJlYWQiLCJ3cml0ZSJdfSwic29fdXNlcl9pZCI6IjgzOTkzIiwic29fdXNlcl9yb2xlIjoidXNlciIsInNvX3Byb2ZpbGUiOiJhbGwiLCJzb191c2VyX25hbWUiOiIiLCJzb19hcGlrZXkiOiJub25lIn0.jqSNF1mJTmWHem1ZfjRQ1WKgsmB5N4tN8OOw7J9h6qI',
       // API_BASE_URL: "http://192.168.1.10:3000/agro-api/salesdash/",
   };
