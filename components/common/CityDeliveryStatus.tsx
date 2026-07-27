47cd188826d1db6c74e655667a328647ac455e9e
Ashen Deelaka <122264429+ashendeelaka@users.noreply.github.com>
2026-07-27 12:34:44 +0530

    Fixed reducing margin bug in addCustomer Screen

--- components/common/CityDeliveryStatus.tsx
+++ components/common/CityDeliveryStatus.tsx
@@ -99,7 +99,7 @@ const CityDeliveryStatus: React.FC<CityDeliveryStatusProps> = ({
         borderRadius: 10,
         paddingVertical: 10,
         paddingHorizontal: 14,
-        marginTop: 18,
+        marginTop: 10,
         borderWidth: 1,
         borderColor: "#FFDCB5",
       }}
